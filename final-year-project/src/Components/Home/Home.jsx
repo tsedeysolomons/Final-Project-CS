513b20f23ccb2a9a50ba31d0fd5fbad7029255c8
eyueldegefa <eyuel144.com>
2024-11-15 17:11:25 +0300

    Student Discount

--- final-year-project/src/Components/Home/Home.jsx
+++ final-year-project/src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import awash from '../../Images/Awash-Woldia1.jpg';
 import djbouti from '../../Images/Djbouti1.jpg';
+import students from '../../Images/students2.jpg';
 import ourTeam from '../../Images/our-team-circle.png';
 import ourTrain from '../../Images/our-train-circle.png';
 import './Home.css';
@@ -47,6 +48,16 @@ function Home() {
                 <p>Awash-weldia</p>
                 <p>Book Economy Class Return until 30 Nov 24</p>
                 <p>from 1,230 ETB</p>
+            </div>
+        </div>
+        <div className='more'><p>See more fares</p></div>
+        <div className='discount'>
+            <img src={students} alt="" />
+            <div className='discountPromo'>
+                <p>SPECIAL OFFER FOR STUDENTS</p>
+                <h3>Students enjoy discounts and extras with us</h3>
+                <p>Book your travel with Ethiopian rail to enjoy special discount, extra baggage allowance and more</p>
+                <button className=''>Learn more</button>
             </div>
         </div>
         <h3>About us</h3>
